e24473f57d4c400461e540c3b5bbc5777d256a80
Wilson Rondini <wilbecoding@gmail.com>
2015-09-16 16:46:12 -0600

    About to get wild with classes

---
+++ public/javascripts/jquery.js
@@ -0,0 +1,18 @@
+ $( document ).ready(function() {
+
+  // $('.snap').on('click', function() {
+  //   console.log("DOES THIS HIT SNAP JQUERY")
+  //   $('.snap').addClass('invisible')
+  // })
+
+  // $('.playImages').on('click', function() {
+  //   $('.topRight').addClass('invisible');
+  //   $('.topLeft').addClass('invisible');
+  //   $('.snap').toggle();
+  //   $('.birdsEyeView').toggle();
+  // });
+});
+
+
+
+    // addClass-removeClass
