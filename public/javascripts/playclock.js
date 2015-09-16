6baa2b5939c4b5ef91506874781270bf7510895a
Wilson Rondini <wilbecoding@gmail.com>
2015-09-15 21:19:32 -0600

    Quazi-Functional play selection buttons

--- public/javascripts/playclock.js
+++ public/javascripts/playclock.js
@@ -1,32 +1,55 @@
-window.onload=playClick;
+window.onload=start;
+
+
+function start() {
+  snap();
+  selectPlay();
+}
+
 
 function playClock(duration, display) {
-                    var timer = duration, minutes, seconds;
+    var timer = duration, minutes, seconds;
 
-                    setInterval(function () {
-                        minutes = parseInt(timer / 60, 10)
-                        seconds = parseInt(timer % 60, 10);
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
-                        minutes = minutes < 10 ? "0" + minutes : minutes;
-                        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-                        display.textContent = seconds;
+        display.textContent = seconds;
 
-                        if (--timer < 0) {
-                            timer = duration;
-                        }
-                    }, 1000);
-                }
-
-
-function playClick() {
-var plays = document.getElementById('playImages');
-var playImg = document.getElementsByClassName('playImages');
-
-    for(var i = 0; i < playImages.length; i++)
-        playImg[i].addEventListener("click", function() {
-        var thirtySeconds = 30;
-        var display = document.querySelector('#playClock')
-        playClock(thirtySeconds, display)
-        })
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
+
+
+function snap() {
+var snapButton = document.getElementsByClassName('snap');
+    console.log("Snap function loads")
+
+    for(var i = 0; i < snapButton.length; i++) {
+        snapButton[i].addEventListener("click", function() {
+            var thirtySeconds = 30;
+            var display = document.querySelector('#playClock')
+            playClock(thirtySeconds, display)
+        })
+    }
+}
+
+function selectPlay() {
+    var playImg = document.getElementsByClassName('playImages');
+    var snapButtonId = document.getElementById('snap');
+
+    console.log("SelectPlay loads");
+    for(var i = 0; i < playImg.length; i++) {
+        playImg[i].addEventListener("click", function() {
+            snapButtonId.className = snapButtonId.className + 'visible' + 'snap';
+            playImg.classname = playImg.classname + 'invisible';
+        })
+    }
+}
+
+
