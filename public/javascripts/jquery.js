42e4e5882fd1e0790a81cd641d80917eb63918d4
Wilson Rondini <wilbecoding@gmail.com>
2015-09-16 22:58:26 -0600

    Field in play. Spent 45m messing with style.scss without gulp watch on. It was hilarious.

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -16,6 +16,48 @@
     console.log("DOES THIS HIT SNAP JQUERY")
     $('.snap').addClass('invisible')
   })
+
+
+  function playClock(duration, display) {
+      var timer = duration, minutes, seconds;
+
+      setInterval(function () {
+          minutes = parseInt(timer / 60, 10)
+          seconds = parseInt(timer % 60, 10);
+
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          display.textContent = seconds;
+          console.log(display);
+          if (--timer < 0) {
+              timer = duration;
+          }
+      }, 1000);
+  }
+
+
+  $('.buttonSnap').on('click', function() {
+      var thirtySeconds = 30;
+      var display = $('#playClock');
+      $('.buttonSnap').css({color: "orange"});
+      console.log(playClock);
+      playClock(thirtySeconds, display)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
--- public/javascripts/playclock.js
+++ public/javascripts/playclock.js
@@ -1,43 +1,42 @@
-window.onload=start;
+// window.onload=start;
 
 
-function start() {
-  snap();
-  // selectPlay();
-}
+// function start() {
+//   snap();
+//   // selectPlay();
+// }
 
 
-function playClock(duration, display) {
-    var timer = duration, minutes, seconds;
+// function playClock(duration, display) {
+//     var timer = duration, minutes, seconds;
 
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = seconds;
+//         display.textContent = seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// function snap() {
+// var snapButton = document.getElementsByClassName('snap');
+// console.log(snapButton);
+//     snapButton.addEventListener("click", function() {
+//         var thirtySeconds = 30;
+//         var display = document.querySelector('#playClock')
+//         playClock(thirtySeconds, display)
+//     })
 
 
-function snap() {
-var snapButton = document.getElementsByClassName('snap');
-    console.log("Snap function loads")
-
-    for(var i = 0; i < snapButton.length; i++) {
-        snapButton[i].addEventListener("click", function() {
-            var thirtySeconds = 30;
-            var display = document.querySelector('#playClock')
-            playClock(thirtySeconds, display)
-        })
-    }
-}
+    
+// }
 
 
 // function selectPlay() {
