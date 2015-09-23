50f9b51b09ed5d07fc3d5f3d067fc6d493c3f873
Wilson Rondini <wilbecoding@gmail.com>
2015-09-23 04:59:10 -0600

    Repositioned playclock. Meh

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -539,7 +539,7 @@ PlayResults = {
     if(PlayResults.WR2OddsAdj + PlayResults.Yards + PlayResults.GlobalOddsAdj >= .2 && PlayResults.WR2OddsAdj + PlayResults.Yards < .7) {
       $('.defensiveSpan').text("Pass to WR2 is complete for an 8 yard gain!");
       $('.footballIMG').animate({'left': '+=6%'}, 'slow');
-      yardLine+=100
+      yardLine+=8
       yardsToFirst -= 8;
     }
     if(PlayResults.WR2OddsAdj + PlayResults.Yards + PlayResults.GlobalOddsAdj >= .7 && PlayResults.WR2OddsAdj + PlayResults.Yards <= .8) {
